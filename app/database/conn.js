79d76bd611990e52ecd527ce645410b85031e118
nitish1728 <56349692+nitish1728@users.noreply.github.com>
2024-12-18 22:13:37 +0530

    Update conn.js
    adding strictquery

--- app/database/conn.js
+++ app/database/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const db = "mongodb+srv://Nitish:Nitish123@interviewbot.0luon.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set('strictQuery', false);
 mongoose.connect(db).then(() => {
     console.log(`Database Connected successfully`);
 }).catch((e) => {
